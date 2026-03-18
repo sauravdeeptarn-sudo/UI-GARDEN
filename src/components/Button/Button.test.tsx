65bd66211622953b2cbad81c576830168bc9c1c6
ssingh214 <ssingh214@rrc.ca>
2026-03-18 10:03:06 -0500

    break lint test

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -11,4 +11,6 @@ test('Button style changes when disabled', () => {
   const { container } = render(<Button label="Click me" disabled />);
   const button = container.querySelector('button');
   expect(button).toHaveStyleRule('opacity', '0.5');
+  var test = "oops";  // eslint should warn about var usage
 });
+
